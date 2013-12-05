faf5eb93bc9355e6ea60f74581549c0a07d19dca
dpfauwadel <d.pfauwadel@technologyandstrategy.com>
2013-12-05 15:53:56 +0100

    Update base64ToJPG.js

--- www/base64ToJPG.js
+++ www/base64ToJPG.js
@@ -1,12 +1,14 @@
-var base64ToJPG = {
+cordova.define("rd.tands.base64ToJPG.base64ToJPG", function(require, exports, module) {var base64ToJPG = {
 	createEvent: function(b64String, params, win, fail) {
 		cordova.exec(
 			win, 
 			fail, 
 			"Base64ToJPG", 
 			"saveImage", 
-			[b64String, params]			
+			[{
+				"b64string" : b64String
+			}]			
 		);
 	}
 }
-module.exports = base64ToJPG;
+module.exports = base64ToJPG;});
